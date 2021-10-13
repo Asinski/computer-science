31e30c3f01ea79f162783b5c247d2aac739a8710
Asinski <pawel.asinski@gmail.com>
2021-10-13 15:33:43 +0300

    add hanoi_towers.py

---
+++ algorithms/other/hanoi_towers.py
@@ -0,0 +1,14 @@
+
+
+def hanoi(n, i, k):
+    if n == 1:
+        print('Диск 1 со штыря {0} на штырь {1}'.format(i, k))
+        return
+    tmp = 6 - i - k
+    hanoi(n - 1, i, tmp)
+    print('Диск {0} со штыря {1} на штырь {2}'.format(n, i, k))
+    hanoi(n - 1, tmp, k)
+
+
+count_disk, pin1, pin2 = map(int, input().split())
+hanoi(count_disk, pin1, pin2)
