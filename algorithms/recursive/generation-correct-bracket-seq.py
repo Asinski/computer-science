a443628241514800b4ec4769f7f262fe5720f953
Asinski <pawel.asinski@gmail.com>
2021-11-20 01:24:42 +0300

    added script

---
+++ algorithms/recursive/generation-correct-bracket-seq.py
@@ -0,0 +1,17 @@
+def bracket(count, s='', left=0, right=0):
+    if left == count and right == count:
+        print(s)
+    else:
+        if left < count:
+            bracket(count, s + '(', left + 1, right)
+        if right < left:
+            bracket(count, s + ')', left, right + 1)
+
+
+def main():
+    n = int(input())
+    bracket(n)
+
+
+if __name__ == "__main__":
+    main()
