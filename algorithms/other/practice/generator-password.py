7eec31c9926989ff13ae504d5a39d1938cab7297
Asinski <76753237+Asinski@users.noreply.github.com>
2021-12-03 12:54:17 +0300

    corrected the typos

--- algorithms/other/practice/generator-password.py
+++ algorithms/other/practice/generator-password.py
@@ -4,12 +4,12 @@ import os
 
 
 letters, numbers, symbols = string.ascii_letters, string.digits, string.punctuation
-all = letters + numbers + symbols
+all_ = letters + numbers + symbols
 
 length = int(input())
 
-password = ''.join(random.sample(all, length))
+password = ''.join(random.sample(all_, length))
 
 os.chdir(os.path.join(os.path.expanduser('~'), 'Desktop'))
 with open("password.txt", "w") as pw:
-    pw.write(password)
+    pw.write(password + '\n')
